5c335ce13cb2bd0770f9b53d07d6bc5a0de6eecb
David Hormigo <dhorram948@g.educaand.es>
2024-03-20 12:43:43 +0100

    Creacion inicial

---
+++ app/src/main/java/com/alanturing/cpifp/whatsappclone/chat/data/Chat.kt
@@ -0,0 +1,6 @@
+package com.alanturing.cpifp.whatsappclone.chat.data
+
+data class Chat(
+    val to:Long, // Identificador de la persona a la que se le escribe
+    val toName: String // Nombre de la persona a la que se le escribe
+)
